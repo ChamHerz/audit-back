8db851dbe835661e24d7520c6aa8a2600568ddc4
chamherz <adrielchambers@gmail.com>
2022-08-11 19:58:42 -0300

    fin de endpoint

--- app.py
+++ app.py
@@ -1,11 +1,73 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+stores = [{
+    'name': "Mi_tienda",
+    'items': [{'name': "Mi_item", 'price': 15.99}]
+}, {
+    'name': "Mi_2da_tienda",
+    'items': [{'name': "Mi_2item", 'price': 65.99}]
+}]
 
 
 @app.route('/')
 def home():
     return 'ApiRest Start'
+
+
+# get /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+    # pass
+
+
+# get /store/<name> data: {name :}
+@app.route('/store/<string:name>')
+def get_store_by_name(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+    # pass
+
+
+# post /store data: {name: }
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+    # pass
+
+
+# put /store data: {name}
+@app.route('/store/<string:name>', methods=['PUT'])
+def update_store(name):
+    for store in stores:
+        if store['name'] == name:
+            request_data = request.get_json()
+            store['name'] = request_data['name']
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+    # pass
+
+
+# delete /store/<name> data: {name}
+@app.route('/store/<string:name>', methods=['DELETE'])
+def delete_store(name):
+    for store in stores:
+        if store['name'] == name:
+            index = stores.index(store)
+            stores.pop(index)
+            return jsonify({'message': 'ok'})
+    return jsonify({'message': 'store not found'})
+    # pass
 
 
 if __name__ == '__main__':
