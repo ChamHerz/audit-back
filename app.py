879212d8488422b96b98c69a8aef5100b31fab5c
chamherz <adrielchambers@gmail.com>
2022-08-11 23:49:40 -0300

    orm

--- app.py
+++ app.py
@@ -1,74 +1,22 @@
-from flask import Flask, jsonify, request
+from flask_restful import Api
+from flask import Flask
+from resources.item import Item
 
 app = Flask(__name__)
-
-stores = [{
-    'name': "Mi_tienda",
-    'items': [{'name': "Mi_item", 'price': 15.99}]
-}, {
-    'name': "Mi_2da_tienda",
-    'items': [{'name': "Mi_2item", 'price': 65.99}]
-}]
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://audit_admin:audit2022@localhost:5433/audit_db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
 
 
-@app.route('/')
-def home():
-    return 'ApiRest Start'
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 
-# get /store
-@app.route('/store')
-def get_stores():
-    return jsonify({'stores': stores})
-    # pass
-
-
-# get /store/<name> data: {name :}
-@app.route('/store/<string:name>')
-def get_store_by_name(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    return jsonify({'message': 'store not found'})
-    # pass
-
-
-# post /store data: {name: }
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
-    # pass
-
-
-# put /store data: {name}
-@app.route('/store/<string:name>', methods=['PUT'])
-def update_store(name):
-    for store in stores:
-        if store['name'] == name:
-            request_data = request.get_json()
-            store['name'] = request_data['name']
-            return jsonify(store)
-    return jsonify({'message': 'store not found'})
-    # pass
-
-
-# delete /store/<name> data: {name}
-@app.route('/store/<string:name>', methods=['DELETE'])
-def delete_store(name):
-    for store in stores:
-        if store['name'] == name:
-            index = stores.index(store)
-            stores.pop(index)
-            return jsonify({'message': 'ok'})
-    return jsonify({'message': 'store not found'})
-    # pass
-
+api.add_resource(Item, '/item/<string:name>')
 
 if __name__ == '__main__':
-    app.run()
+    from db import db
+
+    db.init_app(app)
+    app.run(port=5000, debug=True)
